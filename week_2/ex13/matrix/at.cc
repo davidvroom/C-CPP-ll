97954e5d4bb71ea488b9210ea6d18641c954d207
Unknown <klaasisaac_bijlsma@hotmail.com>
2017-11-26 20:46:34 +0100

    ex13

--- week_2/ex13/matrix/at.cc
+++ week_2/ex13/matrix/at.cc
@@ -1,15 +1,18 @@
 #include "matrix.ih"
 
 double &Matrix::at(size_t rowIdx, size_t colIdx)
-try
 {
-	if (rowIdx >= d_nRows)
-		throw "Row index exceeded";
-	if (colIdx >= d_nCols)
-		throw "Column index exceeded";
-	return el(rowIdx, colIdx);
-}
-catch (char const *message)
-{
-	cerr << "Exception: " << message << '\n';
+	try
+	{
+		if (rowIdx >= d_nRows)
+			throw "Row index exceeded";
+		if (colIdx >= d_nCols)
+			throw "Column index exceeded";
+		return el(rowIdx, colIdx);
+	}
+	catch (char const *message)
+	{
+		cerr << "Exception: " << message << '\n';
+		throw;
+	}
 }
--- week_2/ex13/matrix/transpose.cc
+++ week_2/ex13/matrix/transpose.cc
@@ -4,22 +4,23 @@
 #include "matrix.ih"
 
 Matrix Matrix::transpose() const
-try
 {
-    Matrix ret;                             // optimized implementation
+	Matrix ret;
+	try
+	{
+        ret.d_nCols = d_nRows;                  // prepare the return Matrix
+	    ret.d_nRows = d_nCols;
+	    ret.d_data = new double[size()];
 
-    ret.d_nCols = d_nRows;                  // prepare the return Matrix
-    ret.d_nRows = d_nCols;
-    ret.d_data = new double[size()];
+	    transpose(ret.d_data);
 
-    transpose(ret.d_data);
-
-    return ret;
-}
-catch (...)
-{
-	delete[] ret.d_data;
-	cerr << "Could not make transpose\n";
-	throw;
+	    return ret;
+	}
+	catch (...)
+	{
+		delete[] ret.d_data;
+		cerr << "Could not make transpose\n";
+		throw;
+	}
 }
 
