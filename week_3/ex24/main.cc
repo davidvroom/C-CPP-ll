4c801bd4da6453b95809475cc7e73026964f1d94
Unknown <klaasisaac_bijlsma@hotmail.com>
2017-12-07 23:24:31 +0100

    ex23 af + kleine fixes voor hele pdf

--- week_3/ex24/main.cc
+++ week_3/ex24/main.cc
@@ -4,7 +4,7 @@ int main()
 {
 	string str = "test";
 	size_t count = 3;
-	string *sp = factory(hoi, count);
+	string *sp = factory(str, count);
 	for (size_t idx = 0; idx != count; ++idx)
 		cout << sp[idx] << '\n';
 }
