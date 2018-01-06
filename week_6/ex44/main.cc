671e579663c192afc48ebade2036815acec215c0
Unknown <davidvroom93@hotmail.com>
2018-01-06 18:37:05 +0100

    some adjustments to ex44

--- week_6/ex44/main.cc
+++ week_6/ex44/main.cc
@@ -1,26 +1,42 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-bool vowels(char c, Vstring::Charmap &cmap)
-{
-    
-}
 
 class Vstring: public vector<string>
 {
+    vector<string> d_vs;
+
     public:
         typedef map<char, size_t> Charmap;
 
-        Vstring(istream &in);
+        explicit Vstring(istream &in);
 
         size_t count(Charmap &cmap, bool (*accept)(char, Charmap &));
 
     private:
-        static size_t countChar(string const &str, Charmap &cmap, bool (*accept)(char, Charmap &));
+        //static size_t countChar(string const &str, Charmap &cmap, bool (*accept)(char, Charmap &));
 };
+
+Vstring::Vstring(istream &in)
+{
+    copy(istream_iterator<string>(in), istream_iterator<string>(),
+         back_inserter(d_vs));
+}
+
+inline size_t Vstring::count(Charmap &cmap, bool (*accept)(char, Charmap &))
+{
+    for_each(d_vs.begin(),d_vs.end(),
+             [&os,m](int x) { if (x%m==0) os << x << '\n'; } );
+}
+
+bool vowels(char, Vstring::Charmap &)
+{
+
+}
 
 int main()
 {
