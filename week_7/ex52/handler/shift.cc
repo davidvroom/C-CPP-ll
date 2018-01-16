0b3dcfdf92f519bd9acc5e817e0c1c08086d7840
Unknown <klaasisaac_bijlsma@hotmail.com>
2018-01-16 20:47:45 +0100

    ex52

---
+++ week_7/ex52/handler/shift.cc
@@ -0,0 +1,11 @@
+#include "handler.ih"
+
+void shift(ostream &out, string const &text);
+{
+	cout << text << '\n';
+	for (size_t idx = 1; idx != text.size(); ++idx)
+	{
+		char first = text[0];
+		text.replace(text.begin(), )
+	}
+}
