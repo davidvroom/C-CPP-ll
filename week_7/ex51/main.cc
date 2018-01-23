0250c6ed9d3b49fff143209f5e56f6500e195a18
Unknown <davidvroom93@hotmail.com>
2018-01-23 11:37:36 +0100

    week7

--- week_7/ex51/main.cc
+++ week_7/ex51/main.cc
@@ -1,43 +1,42 @@
-#include <iostream>
-#include <chrono>
-#include <iomanip>
-#include <string.h>
-
-using namespace std;
-using namespace chrono;
+#include "main.ih"
 
 int main(int argc, char **argv)
 {
     					// get the current time
     time_point<system_clock> timePoint{system_clock::now()};
 
-    					// convert it to a std::time_t:
+    printTime(timePoint);
+    
     time_t time = system_clock::to_time_t(timePoint);
 
-    					// display the time:
-    cout << left << setw(14) << "Current time:" 
-         << put_time(localtime(&time), "%c") << '\n';
-    
     					// display the gmtime
-    cout << left << setw(14) << "Gmtime:" 
-         << put_time(gmtime(&time), "%c") << '\n';
+    //cout << put_time(gmtime(&time), "Gmtime: %c\n");
 
     string arg = argv[1];
-    char suffix = arg.back();
     int count = stoi(arg);
 
     					// add or subtract specified time to now
-    if (suffix == 's')
-    	timePoint += seconds(count);
-    else if (suffix == 'm')
-    	timePoint += minutes(count);
-    else if (suffix == 'h')
-    	timePoint += hours(count);
+    switch (char suffix = arg.back())
+    {
+        case 's': 
+            timePoint += seconds(count);
+        break;
+
+        case 'm':
+            timePoint += minutes(count);
+        break;
+
+        case 'h':
+            timePoint += hours(count);
+        break;
+
+        default: 
+            cout << "Invalid suffix\n";
+    }
 
 						// convert it to a std::time_t:
     time_t newTime = system_clock::to_time_t(timePoint);
 
         				// display the time:
-    cout << left << setw(14) << "New time:" 
-         << put_time(localtime(&newTime), "%c") << '\n';
+    cout << put_time(localtime(&newTime), "New time: %c\n");
 }
---
+++ week_7/ex51/printtime.cc
@@ -0,0 +1,15 @@
+#include "main.ih"
+
+void printTime(time_point<system_clock> const &timePoint, TimeMode mode)
+{
+	    				// convert it to a std::time_t:
+    time_t time = system_clock::to_time_t(timePoint);
+
+    if (mode == GMTIME) // display the time:
+    {
+		cout << put_time(gmtime(&time), "Gmtime: %c\n");
+    	return;
+    }
+
+    cout << put_time(localtime(&time), "Current time: %c\n");
+}
